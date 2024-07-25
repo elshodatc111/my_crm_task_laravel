2d3a66bee1b96a78975716b4d03de62f5bad01d5
USER <elshodatc111>
2024-07-25 18:07:37 +0500

    Save

--- app/Http/Controllers/Meneger/ProfelController.php
+++ app/Http/Controllers/Meneger/ProfelController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Meneger;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Models\MarkazHodimStatistka;
+use Illuminate\Support\Facades\Hash;
 
 class ProfelController extends Controller
 {
@@ -13,6 +16,27 @@ class ProfelController extends Controller
         return view('meneger.profel.profel',compact('MarkazHodimStatistka'));
     }
     public function profelUpdatePassword(Request $request){
-        dd($request);
+        $validate = $request->validate([
+            'password' => 'required',
+            'newpassword' => 'required',
+            'confirmpassword' => 'required',
+        ]);
+        $request['email'] = auth()->user()->email;
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            if($request->newpassword == $request->confirmpassword){
+                $User = User::find(auth()->user()->id);
+                $User->password = Hash::make($request->newpassword);
+                $User->save();
+                Auth::guard('web')->logout();
+                $request->session()->invalidate();
+                $request->session()->regenerateToken();
+                return redirect('/login');
+            }else{
+                return redirect()->route('meneger_profel')->with('error', "Yangi parollar mos kelmadi.");
+            }
+        }else{
+            return redirect()->route('meneger_profel')->with('error', "Joriy parol noto'g'ri.");
+        }
     }
 }
--- resources/views/meneger/profel/profel.blade.php
+++ resources/views/meneger/profel/profel.blade.php
@@ -14,6 +14,19 @@
                 </ol>
             </nav>
         </div>
+        @if (Session::has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <i class="bi bi-check-circle me-1"></i>
+                    {{Session::get('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @elseif (Session::has('error'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <i class="bi bi-inbox me-1"></i>
+                    {{Session::get('error') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
         <div class="row">
             <div class="col-lg-4">
                 <div class="card">
